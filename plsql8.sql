079301a39c07e60c1abebeaa62054a2a1dd02a84
yahwinlukose <yahwinlukose@gmail.com>
2025-09-26 10:06:54 +0530

    Delete plsql8.sql

--- plsql8.sql
+++
@@ -1,9 +0,0 @@
-DECLARE
- cursor c1 is select sname,marks from student where marks>=80;
- rec1 c1%rowtype;
- BEGIN
-    for rec1 in c1 LOOP
-         DBMS_OUTPUT.PUT_LINE(rec1.sname||' '||rec1.marks);
-    end loop;
-end;      
-/   
